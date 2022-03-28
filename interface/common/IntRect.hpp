2bbb382b4fe910162726430ce31dd5cc4d757d82
Alexis Cheron <git@norech.com>
2022-03-28 10:36:55 +0400

    added intrect

---
+++ interface/common/IntRect.hpp
@@ -0,0 +1,19 @@
+/**
+ * @version 0.1
+ * @date 2022-03-28
+ */
+
+#pragma once
+
+#include "./ColorCode.hpp"
+
+namespace arc {
+
+struct IntRect {
+    int x;
+    int y;
+    int width;
+    int height;
+};
+
+}  // namespace arc
