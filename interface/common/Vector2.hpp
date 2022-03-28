b5216e3ef1ee12229ff32f7d47131c8087266b21
Alexis Cheron <git@norech.com>
2022-03-28 10:37:04 +0400

    added vector2

---
+++ interface/common/Vector2.hpp
@@ -0,0 +1,17 @@
+/**
+ * @version 0.1
+ * @date 2022-03-28
+ */
+
+#pragma once
+
+#include "./ColorCode.hpp"
+
+namespace arc {
+
+struct Vector2 {
+    int x;
+    int y;
+};
+
+}  // namespace arc
